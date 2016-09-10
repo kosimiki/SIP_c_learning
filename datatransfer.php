ddc246de277481dc293c6f95f05ef922dbfc5629
DESKTOP-3O7NPSD\Gaurri <szkazaattila@gmail.com>
2016-09-10 21:04:42 +0200

    send data from the webpage to the database

--- datatransfer.php
+++ datatransfer.php
@@ -1,16 +1,38 @@
 <?php
-$servername = "sql7.freemysqlhosting.net";
-$username = "sql7134314";
-$password = "hWd6tkFykS";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=sql7134314", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
+	echo "".$_POST["birth"];
+	if ($_POST["username"]==NULL || $_POST["pass"]==NULL || $_POST["fullname"]==NULL || $_POST["mail"]==NULL 
+|| $_POST["sex"]==NULL || $_POST["birth"]==NULL)
+	{
+		echo "The lack of information caused an error";
+	}
+	else
+	{
+	try {
+		$servername = "sql7.freemysqlhosting.net";
+		$dbname= "sql7134314";
+		$username = "sql7134314";
+		$password = "hWd6tkFykS";
+		
+		$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	  
+		$conn->setAttribute(PDO::ATTR_ERRMODE,
+		PDO::ERRMODE_EXCEPTION);
+		$stmt = $conn->prepare("INSERT INTO users (Username, Password,Fullname,Email,Sex,Date_of_birth)
+		VALUES (:Username, :Password , :Fullname , :Email, :Sex , :Date_of_birth)");
+		$stmt->bindParam(':Username', $_POST["username"]);
+		$stmt->bindParam(':Password', $_POST["pass"]);
+		$stmt->bindParam(':Fullname', $_POST["fullname"]);
+		$stmt->bindParam(':Email', $_POST["mail"]);
+		$stmt->bindParam(':Sex', $_POST["sex"]);
+		$stmt->bindParam(':Date_of_birth', $_POST["birth"]);
+		$stmt->execute();
+		echo "New records created successfully";
     }
-catch(PDOException $e)
+	catch(PDOException $e)
     {
-    echo "Connection failed: " . $e->getMessage();
+		echo "Connection failed: " . $e->getMessage();
     }
+		$conn = null;
+	}
+
 ?>
--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <!DOCTYPE HTML>
 <html>  
 <body>
-<?php
-echo "HELLO world"
-?>
+Sign up
 <form action="datatransfer.php" method="post">
-Username: <input type="text" name="name"><br>
+Username: <input type="text" name="username"><br>
 Password: <input type="text" name="pass"><br>
-E-mail: <input type="text" name="mail"><br>
-Gender: <input type="text" name="gend"><br>
-Age: <input type="text" name="age"><br>
+Fullname: <input ype="text" name="fullname"><br>
+E-mail: <input type="email" name="mail"><br>
+Sex: <input type="radio" name="sex" value ="0"> male <input type="radio" name="sex" value ="1"> female<br>
+Birthdate: <input type="date" name="birth"><br>
 <input type="submit">
 </form>
 
