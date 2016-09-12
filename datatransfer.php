ab19b2e5667293e4026859d2680bd0d527c20123
DESKTOP-3O7NPSD\Gaurri <szkazaattila@gmail.com>
2016-09-12 10:25:54 +0200

    High security data management!

--- datatransfer.php
+++ datatransfer.php
@@ -1,30 +1,35 @@
 <?php
-	echo "".$_POST["birth"];
+
 	if ($_POST["username"]==NULL || $_POST["pass"]==NULL || $_POST["fullname"]==NULL || $_POST["mail"]==NULL 
 || $_POST["sex"]==NULL || $_POST["birth"]==NULL)
 	{
 		echo "The lack of information caused an error";
 	}
-	else
+	else		
 	{
 	try {
 		$servername = "sql7.freemysqlhosting.net";
 		$dbname= "sql7134314";
 		$username = "sql7134314";
 		$password = "hWd6tkFykS";
+		$Salt = mcrypt_create_iv(22, MCRYPT_DEV_URANDOM);
+		$options = ['cost' => 9,
+					'salt' => $Salt];
 		
+		$encryptedpassword =password_hash($_POST["pass"], PASSWORD_BCRYPT, $options);
+			
 		$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-	  
 		$conn->setAttribute(PDO::ATTR_ERRMODE,
 		PDO::ERRMODE_EXCEPTION);
-		$stmt = $conn->prepare("INSERT INTO users (Username, Password,Fullname,Email,Sex,Date_of_birth)
-		VALUES (:Username, :Password , :Fullname , :Email, :Sex , :Date_of_birth)");
+		$stmt = $conn->prepare("INSERT INTO users (Username, Password,Fullname,Email,Sex,Date_of_birth,Salt)
+		VALUES (:Username, :Password , :Fullname , :Email, :Sex , :Date_of_birth,:Salt)");
 		$stmt->bindParam(':Username', $_POST["username"]);
-		$stmt->bindParam(':Password', $_POST["pass"]);
+		$stmt->bindParam(':Password', $encryptedpassword);
 		$stmt->bindParam(':Fullname', $_POST["fullname"]);
 		$stmt->bindParam(':Email', $_POST["mail"]);
 		$stmt->bindParam(':Sex', $_POST["sex"]);
 		$stmt->bindParam(':Date_of_birth', $_POST["birth"]);
+		$stmt->bindParam(':Salt',$Salt);
 		$stmt->execute();
 		echo "New records created successfully";
     }
@@ -34,5 +39,4 @@
     }
 		$conn = null;
 	}
-
 ?>
