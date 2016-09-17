b1e75accc9deb3e0e153c310cafee4a7403c4669
DESKTOP-3O7NPSD\Gaurri <szkazaattila@gmail.com>
2016-09-17 12:53:17 +0200

    Validation of username,passworld,email,fullname for empty spaces and special characters

--- index.php
+++ index.php
@@ -3,32 +3,19 @@
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script>
+
 function Bemenet(str,id)
 {
-	var str2='✔';
 	$.post("regvalidation.php",
-        {
+		{
 		col: id,
         text: str
         },
 		
-        function(ifempty){
+        function(respondtext){			
+			$('#'+id).html(respondtext);
 			
-			if (ifempty==str2)
-			{
-			$('#'+id).css('color','#7CFC00');
-			$('#'+id).html(ifempty);
-			}           
-			else
-			{
-			$('#'+id).css('color','#FFA07A');
-			$('#'+id).html(ifempty);
-			}
-			if (ifempy!=str2)
-			{
-			$('#'+id).css('color','#FFA07A');
-			$('#'+id).html(ifempty);	
-			}
+			
 		}
 				
 		);
@@ -44,13 +31,13 @@ function Bemenet(str,id)
 Sign up<br>
 <form action="registration.php" method="post" >
 Username: <input type="text" name="username" onkeyup="Bemenet(this.value,'usernameid')"> <div id="usernameid"></div><br> 
-Password: <input type="text" name="pass" onkeyup="Bemenet(this.value,'passid')"> <div id="passid"></div><br>
+Password: <input type="password" name="pass" onkeyup="Bemenet(this.value,'passid')"> <div id="passid"></div><br>
 Fullname: <input type="text" name="fullname" onkeyup="Bemenet(this.value,'fullnameid')"> <div id="fullnameid"></div><br>
 E-mail: <input type="email" name="mail" onkeyup="Bemenet(this.value,'mailid')"> <div id="mailid"></div><br>
 Sex: <input type="radio" name="sex" value ="0" checked> male 
 <input type="radio" name="sex" value ="1" > female <br>
 Birthdate: <input type="date" name="birth" onkeyup="Bemenet(this.value,'dateid')"> <div id="dateid"></div><br>
-<input type="submit">
+<input type="submit" >
 </form>
 
 </body>
--- regvalidation.php
+++ regvalidation.php
@@ -1,43 +1,116 @@
 <?php
-	$username_column="username";
+	$username_column="username"; 
 	$pass_column="password"; 
 	$fullname_column="fullname";
 	$mail_column="email";
 	$sex_column="sex";
 	$birth_column="date_of_birth";
+	$min_lenght=8;
 	
+		
 	switch ($_POST["col"])
 	{
 		case "usernameid":
-		check_if_exist_or_empty($username_column,$_POST["text"]);
+		{
+		$pattern='/^\S+$/';
+			if (is_valid_size($username_column,$_POST["text"],$pattern))
+		{
+			check_if_exist($username_column,$_POST["text"]);
+		}
+		}
+		
+	
 		
 		break;
 		case "passid":
-		check_if_exist_or_empty($pass_column,$_POST["text"]);
+		{
+		$pattern="/^\S+$/";
+			if (is_valid_size($pass_column,$_POST["text"],$pattern))
+		{
+			check_if_exist($pass_column,$_POST["text"]);
+		}
+		}
+		
 		
 		break;
 		case "fullnameid":
-		
+		{
+		$pattern="/^[a-zA-Z]{1}\X+$/";
+			if (is_valid_size($fullname_column,$_POST["text"],$pattern))
+		{
+			echo '<div style="color:green"> ✔ valid </div>';
+		}			
 		break;
+		}
+		
 		case "mailid":
-		check_if_exist_or_empty($mail_column,$_POST["text"]);
-		email_validation($_POST["text"]);
+		if (email_validation($_POST["text"]))
+		{
+			check_if_exist($mail_column,$_POST["text"]);
+		}
+		
+		
 		
 		break;
 		case "dateid":
-		
+		if (strlen($_POST["text"])>0)
+		{
+			echo '<div style="color:green"> ✔  </div>';
+		}
+			
 		break;	
 	}
+function is_valid_size($column_name,$incomeing_data,$pattern)
+{
+	if (!preg_match($pattern, $incomeing_data))
+	{
+			echo '<div style="color:red"> ✘ Useing unavailable characters </div>';
+			return false;
+	}
+	if ($column_name=="fullname")
+	{
+		return true;
+	}
+	if (strlen($incomeing_data)==0)
+	{
+		echo '<div style="color:red"> ✘ Field is empty  </div>';
+		return false;
+	}
+	if (strlen($incomeing_data)<8)
+	{
+		echo '<div style="color:red"> ✘'.$column_name.' too short</div>';
+		return false;
+	}
+		
+	if (strlen($incomeing_data)>32)
+	{
+		echo '<div style="color:red"> ✘'.$column_name.' too long</div>';
+		return false;
+	}
+	
+	return true;
+		
+	
+		
+}
 function email_validation($incomeing_data)
 {
 	$pattern = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/";
 	if (preg_match($pattern,$incomeing_data))
+	{
 		echo '<div style="color:green"> ✔ email address is valid </div>';
+		return true;
+	}
+		
 	else 
+	{
 		echo '<div style="color:red"> ✘ invalid email address </div>';
+		return false;
+	}
+		
 }		
 
-function check_if_exist_or_empty($column_name,$incomeing_data){
+function check_if_exist($column_name,$incomeing_data){
 	try {		 
 		$servername = "sql7.freemysqlhosting.net";
 		$dbname= "sql7134314";
@@ -56,14 +129,17 @@ function check_if_exist_or_empty($column_name,$incomeing_data){
 		$result = $stmt ->fetchAll();
 		
 		
-		if (sizeof($result)>0)
+		if (sizeof($result)>0 )
 		{		
 			echo '<div style="color:red"> ✘'.$incomeing_data.' alredy in use </div>';
-		}
-        else if($incomeing_data=="")
-			echo '<div style="color:red"> ✘ Field is empty  </div>';
+		}	
 		else 
+		{
+			
 			echo '<div style="color:green"> ✔ unused </div>';
+		}
+			
+		
 		}
 
 	catch(PDOException $e)
